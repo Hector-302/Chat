b023fa25418578b7222658dd6e121a6e0d8105c4
Hector-302 <hectorlv35@gmail.com>
2025-08-16 18:56:49 +0200

    Add user registry service

---
+++ src/main/java/com/example/websocketdemo/service/UserRegistry.java
@@ -0,0 +1,26 @@
+package com.example.websocketdemo.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class UserRegistry {
+
+    private final Map<String, String> users = new ConcurrentHashMap<>();
+
+    public void addUser(String sessionId, String username) {
+        users.put(sessionId, username);
+    }
+
+    public void removeUser(String sessionId) {
+        users.remove(sessionId);
+    }
+
+    public List<String> getAllUsers() {
+        return new ArrayList<>(users.values());
+    }
+}
