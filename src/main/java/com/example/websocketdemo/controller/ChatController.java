63a7a8e6920b289749982e790263574ac42ce6b8
Hector-302 <hectorlv35@gmail.com>
2025-08-19 18:24:38 +0200

    Update user registry on connect

--- src/main/java/com/example/websocketdemo/controller/ChatController.java
+++ src/main/java/com/example/websocketdemo/controller/ChatController.java
@@ -1,10 +1,12 @@
 package com.example.websocketdemo.controller;
 
 import com.example.websocketdemo.model.ChatMessage;
+import com.example.websocketdemo.service.ServicesUserRegistry;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -12,6 +14,15 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class ChatController {
+
+    private final ServicesUserRegistry userRegistry;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public ChatController(ServicesUserRegistry userRegistry,
+                          SimpMessagingTemplate messagingTemplate) {
+        this.userRegistry = userRegistry;
+        this.messagingTemplate = messagingTemplate;
+    }
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
@@ -25,6 +36,11 @@ public class ChatController {
                                SimpMessageHeaderAccessor headerAccessor) {
         // Add username in web socket session
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+
+        // Register user and broadcast updated list
+        userRegistry.addUser(headerAccessor.getSessionId(), chatMessage.getSender());
+        messagingTemplate.convertAndSend("/topic/users", userRegistry.getAllUsers());
+
         return chatMessage;
     }
 
